6020971530b452820c7b1ae5963c69a21a279a85
sampath88 <sampathreddy8896@gmail.com>
2023-11-29 21:52:26 +0530

    home page check

---
+++ src/pages/home-page.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <br />
+      <button>Login</button>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/router.jsx
+++ src/router.jsx
@@ -1,7 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/login-page";
+import HomePage from "./pages/home-page";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
   {
     path: "/login",
     element: <LoginPage />,
