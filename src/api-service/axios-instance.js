48134917c7de7dbe41e0f3cccbaba58922724b83
sampath88 <sampathreddy8896@gmail.com>
2024-01-06 02:23:45 +0530

    env update

--- src/api-service/axios-instance.js
+++ src/api-service/axios-instance.js
@@ -8,8 +8,7 @@ import {
 import { isArray } from "src/utils/utils";
 
 const isProduction = import.meta.env.PROD;
-const prod = "https://perfectresume-ai.onrender.com";
-const prod2 = "https://perfectresume-ai-4zbw.onrender.com";
+const prod = import.meta.env.VITE_API;
 const dev = "http://localhost:3000";
 
 const backend = isProduction ? prod : dev;
